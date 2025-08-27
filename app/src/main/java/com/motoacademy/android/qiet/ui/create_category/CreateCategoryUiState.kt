b56468145e5e25ea7b620e9d3859d5630add1dc3
Franklin Neto <2frneto@gmail.com>
2025-08-27 16:41:01 -0400

    QIET-17 add class CreateCategoryUiState

---
+++ app/src/main/java/com/motoacademy/android/qiet/ui/create_category/CreateCategoryUiState.kt
@@ -0,0 +1,15 @@
+package com.motoacademy.android.qiet.ui.create_category
+
+data class CreateCategoryUiState (
+    val categoryName: String = "",
+    val categoryColorHex: String = "#FFFFFF",
+    val blockedPrefixes: List<String> = emptyList(),
+    val blockedContacts: List<String> = emptyList(),
+    val timeRestrictionsEnabled: Boolean = false,
+    val startTime: String = "08:00",
+    val endTime: String = "22:00",
+    val weekDays: Set<String> = emptySet(),
+    val isSaving: Boolean = false,
+    val error: String? = null
+
+)
