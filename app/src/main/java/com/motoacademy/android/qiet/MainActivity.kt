d4740cd3239b4cb6afa4f0f00bb89deba2d6d3f9
Felipe Palma <felipepalma14@gmail.com>
2025-08-26 22:58:47 -0400

    [QIET-20] Add block rule item component (#8)
    * [ADDED][QIET-7] Add mockk dependencies (#2)
    
    * [ADDED][QIET-6] Add hilt dependencies and configurations (#1)
    
    * add retrofit related libs
    
    * [QIET-8] Add GitHub Actions [CI/CD] (#4)
    
    * [QIET-8] Add GitHub Actions [CI/CD]
    
    * [QIET-8] Add GitHub Actions [CI/CD]
    
    * [QIET-8] Add GitHub Actions [CI/CD]
    
    * [QIET-8] Add GitHub Actions [CI/CD]
    
    * Add compose libraries
    
    * config METa-INF
    
    * [QIET-25] Add jacoco config (#5)
    
    * [QIET-25] Add jacoco config
    
    * [QIET-25] Add jacoco config
    
    * [QIET-26] Add bottom navigation (#6)
    
    * [QIET-20] Add block rule item component
    
    ---------
    
    Co-authored-by: Franklin Neto <2frneto@gmail.com>
    Co-authored-by: Franklin Ferreira Rezende Neto <57413372+FranklinNeto@users.noreply.github.com>

---
+++ app/src/main/java/com/motoacademy/android/qiet/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.motoacademy.android.qiet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.rememberNavController
+import com.motoacademy.android.qiet.ui.components.navigation.BottomNavigationBar
+import com.motoacademy.android.qiet.ui.components.navigation.NavHostContainer
+import com.motoacademy.android.qiet.ui.theme.QietTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QietTheme {
+                val navController = rememberNavController()
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.White)
+                ) {
+                    Scaffold(
+                        floatingActionButton = {
+                            FloatingActionButton(onClick = { }) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = "add"
+                                )
+                            }
+                        },
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .statusBarsPadding()
+                            .navigationBarsPadding(),
+                        bottomBar = {
+                            BottomNavigationBar(
+                                navController = navController
+                            )
+                        }
+                    ) { innerPadding ->
+                        NavHostContainer(
+                            modifier = Modifier
+                                .padding(innerPadding),
+                            navController = navController
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/motoacademy/android/qiet/ui/components/navigation/NavHostController.kt
@@ -0,0 +1,30 @@
+package com.motoacademy.android.qiet.ui.components.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.motoacademy.android.qiet.features.dashboard.BlockDashboardScreen
+
+@Composable
+fun NavHostContainer(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+) {
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.BlockDashboardScreen,
+        modifier = modifier,
+        builder = {
+            composable<Screen.BlockDashboardScreen> {
+                BlockDashboardScreen(navController = navController)
+            }
+
+            composable<Screen.HistoryScreen> {
+
+            }
+        }
+    )
+}
