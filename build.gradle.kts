0ec176f415e3bb16a1c142a0be996d9fc5d7c903
Felipe Palma <felipepalma14@gmail.com>
2025-08-26 22:59:28 -0400

    Revert "[QIET-20] Add block rule item component (#8)" (#9)
    This reverts commit d4740cd3239b4cb6afa4f0f00bb89deba2d6d3f9.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,4 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.hilt) apply false
-    alias(libs.plugins.ksp) apply false
-}
-
-buildscript {
-    dependencies {
-        classpath(libs.org.jacoco.core) // Use the latest version
-    }
-    repositories {
-        google()
-    }
 }
