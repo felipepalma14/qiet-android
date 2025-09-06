a47493b8a8368dde59408c13224fec0e316db0a0
Felipe Palma <felipepalma14@gmail.com>
2025-09-06 17:45:50 -0400

    Improvements CI/CD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,13 @@ android {
         }
 
     }
+
+    lint {
+        checkAllWarnings = true  // se verdadeiro, verificar todos os problemas, incluindo aqueles que estão desativados por padrão
+        abortOnError = true  // se verdadeiro, interromper a compilação do Gradle se forem encontrados erros
+        warningsAsErrors = false  // se verdadeiro, tratar todos os avisos como erros
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -64,101 +71,189 @@ android {
         unitTests.isReturnDefaultValues = true
     }
 
-    tasks.register<JacocoReport>("generateJacocoCoverageReport") {
-        group = "Reporting"
-        description = "Generate JaCoCo coverage report for the debug build."
+//    tasks.register<JacocoReport>("jacocoTestReport") {
+//        group = "Reporting"
+//        description = "Generate JaCoCo coverage report for the debug build."
+//
+//        dependsOn("testDebugUnitTest")
+//
+//        finalizedBy(tasks.named("jacocoCoverageVerification").get())
+//
+//        reports {
+//            xml.required.set(true)
+//            html.required.set(true)
+//            csv.required.set(false)
+//        }
+//
+//        val fileFilter = listOf(
+//            "**/*ScreenKt.class",
+//            "**/*Activity.class",
+//            "**/*ActivityKt*",
+//            "**/*Fragmnt.class",
+//            "**/*FragmentKt*",
+//            "**/dagger/**",
+//            "**/hilt/**",
+//            "**/generated/**",
+//            "**/*_HiltComponents*",
+//            "**/*Dagger*",
+//            "**/*Hilt*",
+//            "**/*\$*",
+//            "**/*_Factory*",
+//            "**/*_Impl*",
+//            "**/*_MembersInjector*",
+//            "**/*_Module*",
+//            "**/*_Subcomponent*",
+//            "**/*_Component*",
+//            "**/theme/**",
+//            "**/components/**",
+//            "**/navigation/**"
+//        )
+//
+//        val javaClasses = fileTree("${project.buildDir}/tmp/kotlin-classes/debug") {
+//            exclude(fileFilter)
+//        }
+//
+//        val kotlinClasses = fileTree("${project.buildDir}/tmp/kotlin-classes/debug") {
+//            exclude(fileFilter)
+//        }
+//
+//        val sourceDirs = listOf("src/main/java", "src/main/kotlin")
+//
+//        classDirectories.setFrom(files(javaClasses, kotlinClasses))
+//        additionalSourceDirs.setFrom(files(sourceDirs))
+//        sourceDirectories.setFrom(files(sourceDirs))
+//
+//        val execFiles = fileTree("$buildDir") {
+//            include("**/*.exec")
+//        }
+//        executionData.setFrom(execFiles)
+//    }
+//
+//    tasks.register<JacocoCoverageVerification>("jacocoCoverageVerification") {
+//
+//        // classDirectories e executionData devem ser os mesmos do generateJacocoCoverageReport
+//        val reportTask = tasks.named<JacocoReport>("jacocoTestReport").get()
+//
+//        classDirectories.setFrom(reportTask.classDirectories)
+//        executionData.setFrom(reportTask.executionData)
+//
+//        violationRules {
+//            rule {
+//                limit {
+//                    counter = "LINE"
+//                    value = "COVEREDRATIO"
+//                    // mínimo de 80% de cobertura
+//                    minimum = "0.8".toBigDecimal()
+//                }
+//            }
+//        }
+//    }
+//
+//    tasks.register<TestReport>("unitTests") {
+//        description = "Generates a combined test report for all modules"
+//        group = "verification"
+//
+//        // Coleta os resultados de todos os módulos
+//        val results = subprojects.mapNotNull { subproject ->
+//            subproject.tasks.findByName("testDebugUnitTest")?.let { task ->
+//                dependsOn(task)
+//                (task as? Test)?.binaryResultsDirectory
+//            }
+//        }
+//
+//        testResults.from(results)
+//        // Caminho do relatório agregado
+//        destinationDirectory.set(layout.buildDirectory.dir("reports/tests/aggregate"))
+//    }
 
-        dependsOn("testDebugUnitTest")
-
-        finalizedBy(tasks.named("jacocoCoverageVerification").get())
-
-        reports {
-            xml.required.set(true)
-            html.required.set(true)
-            csv.required.set(false)
-        }
-
-        val fileFilter = listOf(
-            "**/*ScreenKt.class",
-            "**/*Activity.class",
-            "**/*ActivityKt*",
-            "**/*Fragmnt.class",
-            "**/*FragmentKt*",
-            "**/dagger/**",
-            "**/hilt/**",
-            "**/generated/**",
-            "**/*_HiltComponents*",
-            "**/*Dagger*",
-            "**/*Hilt*",
-            "**/*\$*",
-            "**/*_Factory*",
-            "**/*_Impl*",
-            "**/*_MembersInjector*",
-            "**/*_Module*",
-            "**/*_Subcomponent*",
-            "**/*_Component*",
-            "**/theme/**",
-            "**/components/**",
-            "**/navigation/**"
-        )
-
-        val javaClasses = fileTree("${project.buildDir}/tmp/kotlin-classes/debug") {
-            exclude(fileFilter)
-        }
-
-        val kotlinClasses = fileTree("${project.buildDir}/tmp/kotlin-classes/debug") {
-            exclude(fileFilter)
-        }
-
-        val sourceDirs = listOf("src/main/java", "src/main/kotlin")
-
-        classDirectories.setFrom(files(javaClasses, kotlinClasses))
-        additionalSourceDirs.setFrom(files(sourceDirs))
-        sourceDirectories.setFrom(files(sourceDirs))
-
-        val execFiles = fileTree("$buildDir") {
-            include("**/*.exec")
-        }
-        executionData.setFrom(execFiles)
-    }
-
-    tasks.register<JacocoCoverageVerification>("jacocoCoverageVerification") {
-        // classDirectories e executionData devem ser os mesmos do generateJacocoCoverageReport
-        val reportTask = tasks.named<JacocoReport>("generateJacocoCoverageReport").get()
-
-        classDirectories.setFrom(reportTask.classDirectories)
-        executionData.setFrom(reportTask.executionData)
-
-        violationRules {
-            rule {
-                limit {
-                    counter = "LINE"
-                    value = "COVEREDRATIO"
-                    // mínimo de 80% de cobertura
-                    minimum = "0.8".toBigDecimal()
-                }
-            }
-        }
-    }
-
-    tasks.register<TestReport>("unitTests") {
-        description = "Generates a combined test report for all modules"
-        group = "verification"
-
-        // Coleta os resultados de todos os módulos
-        val results = subprojects.mapNotNull { subproject ->
-            subproject.tasks.findByName("testDebugUnitTest")?.let { task ->
-                dependsOn(task)
-                (task as? Test)?.binaryResultsDirectory
-            }
-        }
-
-        testResults.from(results)
-        // Caminho do relatório agregado
-        destinationDirectory.set(layout.buildDirectory.dir("reports/tests/aggregate"))
-    }
 
 }
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+val fileFilter = listOf(
+    "**/*ScreenKt.class",
+    "**/*Activity.class",
+    "**/*ActivityKt*",
+    "**/*Fragmnt.class",
+    "**/*FragmentKt*",
+    "**/dagger/**",
+    "**/hilt/**",
+    "**/generated/**",
+    "**/*_HiltComponents*",
+    "**/*Dagger*",
+    "**/*Hilt*",
+    "**/*\$*",
+    "**/*_Factory*",
+    "**/*_Impl*",
+    "**/*_MembersInjector*",
+    "**/*_Module*",
+    "**/*_Subcomponent*",
+    "**/*_Component*",
+    "**/theme/**",
+    "**/components/**",
+    "**/navigation/**"
+)
+
+val debugTree = fileTree(
+    mapOf(
+        "dir" to "${buildDir}/intermediates/javac/debug",
+        "excludes" to fileFilter
+    )
+)
+
+val kotlinDebugTree = fileTree(
+    mapOf(
+        "dir" to "${buildDir}/tmp/kotlin-classes/debug",
+        "excludes" to fileFilter
+    )
+)
+
+val mainSrc = files("src/main/java", "src/main/kotlin")
+
+tasks.register<JacocoReport>("jacocoTestReport") {
+    dependsOn("testDebugUnitTest")
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+
+    sourceDirectories.setFrom(mainSrc)
+    classDirectories.setFrom(files(debugTree, kotlinDebugTree))
+    executionData.setFrom(fileTree(buildDir) {
+        include("**/jacoco/testDebugUnitTest.exec")
+    })
+}
+
+tasks.register<JacocoCoverageVerification>("jacocoCoverageVerification") {
+    dependsOn("testDebugUnitTest")
+
+    sourceDirectories.setFrom(mainSrc)
+    classDirectories.setFrom(files(debugTree, kotlinDebugTree))
+    executionData.setFrom(fileTree(buildDir) {
+        include("**/jacoco/testDebugUnitTest.exec")
+    })
+
+    violationRules {
+        rule {
+            limit {
+                counter = "LINE"
+                value = "COVEREDRATIO"
+                minimum = "0.80".toBigDecimal() // require at least 80% line coverage
+            }
+        }
+    }
+}
+
+// hook into `check` so CI/CD fails if coverage is low
+tasks.named("check") {
+    dependsOn("jacocoTestReport", "jacocoCoverageVerification")
+}
+
 
 dependencies {
 
@@ -168,8 +263,6 @@ dependencies {
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
     kapt(libs.room.compiler)
-    implementation(libs.mockk)
-    implementation(libs.mockk.android)
 
     // Hilt
     implementation(libs.hilt.android)
@@ -201,6 +294,10 @@ dependencies {
     implementation(libs.androidx.material3)
 
     testImplementation(libs.junit)
+    testImplementation(libs.mockk.agent)
+    testImplementation(libs.mockk)
+    testImplementation(libs.mockk.android)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
