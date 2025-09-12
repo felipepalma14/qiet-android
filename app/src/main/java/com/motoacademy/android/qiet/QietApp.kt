20b2914b7c339e8ea8226ad5f7066c10deff3c8e
Franklin Neto <2frneto@gmail.com>
2025-09-12 01:31:30 -0400

    QIET-46 add StringListConverterTest

--- app/src/main/java/com/motoacademy/android/qiet/QietApp.kt
+++ app/src/main/java/com/motoacademy/android/qiet/QietApp.kt
@@ -2,15 +2,14 @@ package com.motoacademy.android.qiet
 
 import android.app.Application
 import com.motoacademy.android.qiet.di.ApplicationComponent
-import com.motoacademy.android.qiet.di.DaggerApplicationComponent
+//import com.motoacademy.android.qiet.di.DaggerApplicationComponent
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class QietApp : Application() {
-    lateinit var applicationComponent: ApplicationComponent
-
-    override fun onCreate() {
-        super.onCreate()
-        applicationComponent = DaggerApplicationComponent.factory().create(this)
-    }
-}
+class QietApp : Application()
+//    lateinit var applicationComponent: ApplicationComponent
+//    override fun onCreate() {
+//        super.onCreate()
+//        applicationComponent = DaggerApplicationComponent.factory().create(this)
+//    }
+//}
