8a0462b90d9f17a0b64194443f7537d8290c333a
Franklin Neto <2frneto@gmail.com>
2025-09-12 04:42:29 -0400

    QIET-46 add queries of insert and get all BlockRule Entity in BlockRuleDao

--- app/src/main/java/com/motoacademy/android/qiet/data/local/dao/BlockRuleDao.kt
+++ app/src/main/java/com/motoacademy/android/qiet/data/local/dao/BlockRuleDao.kt
@@ -2,16 +2,28 @@ package com.motoacademy.android.qiet.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.OnConflictStrategy
+import kotlinx.coroutines.flow.Flow
 import com.motoacademy.android.qiet.data.local.entity.BlockRuleEntity
 
 @Dao
 interface BlockRuleDao {
 
     //insert
-   // @Insert(onConflict = onConflictStrategy.REPLACE)
-    suspend fun insert(rule: BlockRuleEntity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(rule: BlockRuleEntity): Long //retorna o id da regra adicionada
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(rules:List<BlockRuleEntity>): List<Long> //retorna lista de ids das regras
 
     //select
+    @Query("SELECT * FROM block_rule")
+    fun getAllWithFlow(): Flow<List<BlockRuleEntity>> //pedir explicacao para felipe
+
+    @Query("SELECT * FROM block_rule")
+    suspend fun getAllOnce(): List<BlockRuleEntity>
+
 
     //delete
 
